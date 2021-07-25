683011b90a1441ef16fa5c3fe91f810c5ae4fd12
Awni <Wintrygamer@gmail.com>
2021-07-25 19:32:29 +0200

    Change route file name and update app
    Change from 'products' to 'food' as route file name and update app to reflect the changes.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
-import products from './routes/products';
+import food from './routes/food';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/products', products);
+app.use('/food', food);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);

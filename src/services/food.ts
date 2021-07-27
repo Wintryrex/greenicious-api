3a6fc0755942a5bf420afb64ce93f583798b503c
Awni <Wintrygamer@gmail.com>
2021-07-28 00:57:04 +0200

    Implement read all method
    Create a feature that retrieve all the rows from the database.

--- src/services/food.ts
+++ src/services/food.ts
@@ -1,7 +1,12 @@
 import Food from '../types/food';
+import FoodModel from '../models/food';
 
 class FoodService {
-  async getAll() {}
+  async getAll(): Promise<Food[]> {
+    const items: Food[] = await FoodModel.find({});
+    return items;
+  }
+
   async add(item: Food) {}
   async get(id: string | number) {}
   async update(id: string | number, item: Food) {}

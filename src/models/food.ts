2359af657cab7e89da43eff7345fad126624f66e
Awni <Wintrygamer@gmail.com>
2021-07-30 00:16:56 +0200

    Auto create document
    Auto create document if it does not exist during start of the application

--- src/models/food.ts
+++ src/models/food.ts
@@ -1,15 +1,18 @@
 import { Schema, model } from 'mongoose';
 import Product from '../types/food';
 
-const schema = new Schema<Product>({
-  title: { type: String, required: true },
-  ingrediens: { type: String, required: true },
-  stores: { type: [String], required: true },
-  measurement: {
-    unit: { type: String, required: true },
-    amount: { type: Number, required: true },
+const schema = new Schema<Product>(
+  {
+    title: { type: String, required: true },
+    ingrediens: { type: String, required: true },
+    stores: { type: [String], required: true },
+    measurement: {
+      unit: { type: String, required: true },
+      amount: { type: Number, required: true },
+    },
   },
-});
+  { autoCreate: true }
+);
 
 const Food = model<Product>('Food', schema);
 

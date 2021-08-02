b6c2650d2f01da49d6ce631cf3eb13063ce8d0c0
Awni <Wintrygamer@gmail.com>
2021-08-02 17:55:48 +0200

    Correct the return type on the try catch handler

--- src/middlewares/trycatch-handler.ts
+++ src/middlewares/trycatch-handler.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 
 const tryCatchHandler = (
-  action: (req: Request, res: Response) => void
+  action: (req: Request, res: Response) => Promise<void>
 ): RequestHandler => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
